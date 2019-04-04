53d6d231b956f335104263353caec424298d0148
david <david@appsaloon.be>
2019-04-04 18:03:09 +0200

    added ajax calls to add variations to cart

--- config/show-product-variations-table.php
+++ config/show-product-variations-table.php
@@ -13,12 +13,54 @@ class Show_Product_Variations_Table {
 		// Enqueue scripts and styles
 		add_action( 'wp_enqueue_scripts', array( $this, 'variations_table_scripts' ) );
 
+
+		// Add ajax callback to add variation to cart
+		add_action( 'wp_ajax_variation_add_to_cart', array( $this, 'variations_table_ajax_variation_add_to_cart' ) );
+		add_action( 'wp_ajax_nopriv_variation_add_to_cart', array(
+			$this,
+			'variations_table_ajax_variation_add_to_cart'
+		) );
+	}
+
+	function variations_table_get_variation_data_from_variation_id( $variation_id ) {
+		$_product       = new \WC_Product_Variation( $variation_id );
+		$variation_data = $_product->get_variation_attributes();
+
+		return $variation_data; // $variation_data will return only the data which can be used to store variation data
+	}
+
+	function variations_table_ajax_variation_add_to_cart() {
+		ob_start();
+		$product_id   = apply_filters( 'vartable_add_to_cart_product_id', absint( $_POST['product_id'] ) );
+		$quantity     = empty( $_POST['quantity'] ) ? 1 : wc_stock_amount( $_POST['quantity'] );
+		$variation_id = isset( $_POST['variation_id'] ) ? absint( $_POST['variation_id'] ) : '';
+		$variations   = $this->variations_table_get_variation_data_from_variation_id( $variation_id );
+
+		$passed_validation = apply_filters( 'vartable_add_to_cart_validation', true, $product_id, $quantity, $variation_id, $variations );
+
+		if ( $passed_validation && WC()->cart->add_to_cart( $product_id, $quantity, $variation_id, $variations ) ) {
+			do_action( 'vartable_ajax_added_to_cart', $product_id );
+			if ( get_option( 'woocommerce_cart_redirect_after_add' ) == 'yes' ) {
+				wc_add_to_cart_message( $product_id );
+			}
+			// Return fragments
+			\WC_AJAX::get_refreshed_fragments();
+		} else {
+			// If there was an error adding to the cart, redirect to the product page to show any errors
+			$data = array(
+				'error'       => true,
+				'product_url' => apply_filters( 'woocommerce_cart_redirect_after_error', get_permalink( $product_id ), $product_id )
+			);
+			wp_send_json( $data );
+		}
+
+		die();
 	}
 
 	public function variations_table_scripts() {
 		if ( is_product() ) {
-			wp_enqueue_script( 'woo-product-variations-table-style', WOO_PVT_URL.'js/woo-product-variations-table.js', array(), WOO_PVT_VERSION, true );
-			wp_enqueue_style( 'woo-product-variations-table-style', WOO_PVT_URL.'css/woo-product-variations-table.css', array(), WOO_PVT_VERSION );
+			wp_enqueue_script( 'woo-product-variations-table-style', WOO_PVT_URL . 'js/woo-product-variations-table.js', array(), WOO_PVT_VERSION, true );
+			wp_enqueue_style( 'woo-product-variations-table-style', WOO_PVT_URL . 'css/woo-product-variations-table.css', array(), WOO_PVT_VERSION );
 		}
 	}
 
@@ -28,8 +70,8 @@ class Show_Product_Variations_Table {
 
 	public function show_product_variations() {
 		global $product;
-		$productId = $product->get_id();
+		$productId   = $product->get_id();
 		$apiEndPoint = Product::getApiEndpoint() . $productId;
-		echo "<div id='variations-table' class='variations-table' data-ajax-url='admin-ajax.php?add_variation_to_cart=1' data-api-endpoint='" . $apiEndPoint . "' data-product-id='" . $productId . "' >test</div>";
+		echo "<div id='variations-table' class='variations-table' data-add-to-cart-url='/wp/wp-admin/admin-ajax.php?add_variation_to_cart=1' data-api-endpoint='" . $apiEndPoint . "' data-product-id='" . $productId . "' >test</div>";
 	}
 }
