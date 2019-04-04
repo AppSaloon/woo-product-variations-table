2648582539c69de0aaef4c84b93848989eee0607
david <david@appsaloon.be>
2019-04-04 11:35:22 +0200

    added div root element for react component + css

--- config/show-product-variations-table.php
+++ config/show-product-variations-table.php
@@ -2,11 +2,25 @@
 
 namespace woo_pvt\config;
 
+use woo_pvt\model\Product;
+
 class Show_Product_Variations_Table {
 
 	public function register_product_variations_table() {
 		add_action( 'plugins_loaded', array( $this, 'remove_variable_product_add_to_cart' ) );
 		add_filter( 'woocommerce_after_single_product_summary', array( $this, 'show_product_variations' ), 9 );
+
+		// Enqueue scripts and styles
+		add_action( 'wp_enqueue_scripts', array( $this, 'variations_table_scripts' ) );
+
+	}
+
+	public function variations_table_scripts() {
+		if ( is_product() ) {
+			wp_enqueue_script( 'woo-product-variations-table-style', WOO_PVT_URL.'js/woo-product-variations-table.js', array(), WOO_PVT_VERSION );
+//			wp_enqueue_script( 'woo-product-variations-table-style', WOO_PVT_URL.'js/woo-product-variations-table.js', array(), WOO_PVT_VERSION, true );
+			wp_enqueue_style( 'woo-product-variations-table-style', WOO_PVT_URL.'css/woo-product-variations-table.css', array(), WOO_PVT_VERSION );
+		}
 	}
 
 	public function remove_variable_product_add_to_cart() {
@@ -14,6 +28,9 @@ class Show_Product_Variations_Table {
 	}
 
 	public function show_product_variations() {
-		echo "todo build the table for the product variations";
+		global $product;
+		$productId = $product->get_id();
+		$apiEndPoint = Product::getApiEndpoint() . $productId;
+		echo "<div id='variations-table' class='variations-table' data-api-endpoint='" . $apiEndPoint . "' data-product-id='" . $productId . "' >test</div>";
 	}
 }
